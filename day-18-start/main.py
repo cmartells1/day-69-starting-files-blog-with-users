1af0d1a33fae26ead6e2d592f67caf0dfbafc082
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-06-29 15:23:37 -0600

    random colors with Tuples

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -1,7 +1,9 @@
+import turtle as t
 from turtle import Turtle, Screen
 import random
 
-tim = Turtle()
+tim = t.Turtle()
+t.colormode(255)
 # timmy_the_turtle.shape("turtle")
 # timmy_the_turtle.color("red", "purple")
 # timmy_the_turtle.forward(100)
@@ -13,8 +15,6 @@ tim = Turtle()
 #     tim.forward(10)
 #     tim.pendown()
 
-colours = ["blue", "red", "purple", "magenta"]
-
 # def draw_shape(num_sides):
 #     angle = 360/ num_sides
 #     for _ in range(num_sides):
@@ -25,12 +25,20 @@ colours = ["blue", "red", "purple", "magenta"]
 #     tim.color(random.choice(colours))
 #     draw_shape(shape_side_n)
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
 for _ in range(200):
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
