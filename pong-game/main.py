5ee6a30a53db0afd319db93867a0cad7945ca9ab
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-07-13 15:55:17 -0600

    create ball class

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
-
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -11,6 +12,8 @@ screen.tracer(0)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 
+ball = Ball()
+
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -19,7 +22,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.050)
     screen.update()
+    ball.move()
 
 
 
