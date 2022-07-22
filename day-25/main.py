7af353d812c6a0b7ef6fe566ccb4f1e3b45173a3
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-07-22 00:33:09 -0600

    data frames and series

--- day-25/main.py
+++ day-25/main.py
@@ -13,5 +13,33 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
+# print(data["temp"])
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# print(data.condition)
+
+#get data in row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+# monday_temp_f = monday_temp * 9/5 + 32
+# print(monday_temp_f)
+
+#Create a data from
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76,56,65]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
+
