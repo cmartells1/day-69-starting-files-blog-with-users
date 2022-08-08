0fdcffec2b7c2c1ffd5645c49d8f813098c1b9e4
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-08 15:57:44 -0600

    Add a count down mechanism

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,9 +12,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5* 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    #this would change the canvas it is different as you have to grab the canvas you want to change
+    #and then connect to the thing you want to change inside the assigned variable
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0 :
+        window.after(1000, count_down, count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,8 +35,9 @@ window.config(pady=50, padx=100, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=2)
+
 
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 timer_label.grid(column=1, row=0)
@@ -32,7 +45,7 @@ timer_label.grid(column=1, row=0)
 checkmarks = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
 checkmarks.grid(column=1, row=4)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=3)
 
 reset_button = Button(text="Reset")
