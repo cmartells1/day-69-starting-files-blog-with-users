263b05d4157e249fc5a5785bac56bddf74c90c81
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-08 14:04:57 -0600

    tkinter layout managers

--- day-27/main.py
+++ day-27/main.py
@@ -2,32 +2,35 @@
 from tkinter import *
 
 
+def button_clicked():
+    # my_label["text"] = "Button got clicked."
+    new_text = input.get()
+    my_label.config(text=new_text)
+
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 #label
 
 my_label = Label(text="I am a label", font=("Arial", 24,"bold"))
 #label won't show up unless you use the pack to pack it on to the screen
-my_label.pack()
-
-my_label["text"] = "New Text"
 my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+
 
 #Button
-
-def button_clicked():
-    # my_label["text"] = "Button got clicked."
-    new_text = input.get()
-    my_label.config(text=new_text)
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
 #Entry
 
 input = Entry(width=10)
-input.pack()
+input.grid(column=3, row=2)
 
 
 
