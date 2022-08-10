901cccfa8ee45f670bb050435e98f3f3bcd30db9
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-10 12:50:27 -0600

    saving data to a file

--- password-manager/main.py
+++ password-manager/main.py
@@ -3,7 +3,17 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
 
+    with open("./data.txt",mode="a") as data:
+        data.write(f"{website} | {email} | {password} \n")
+
+
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -18,13 +28,15 @@ canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-website_entry = Entry(width=35, justify="left")
+website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+website_entry.focus()
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+email_entry.insert(0, "cmartells2@gmail.com")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
@@ -34,7 +46,7 @@ password_entry.grid(column=1, row=3, sticky="EW")
 password_button = Button(text="Generate Password")
 password_button.grid(column=2, row=3, sticky="EW")
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 
