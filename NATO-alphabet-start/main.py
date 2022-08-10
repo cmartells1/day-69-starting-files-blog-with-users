1289a1b0d2b97eb277874342ff680c00a49df3ab
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-10 14:24:50 -0600

    exception handling on NATO program

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,33 +1,41 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+#
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+#
+# #TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
 
 nato_alphabet_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alpha_dict = {row.letter: row.code for (index, row) in nato_alphabet_data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+#
+def generate_phonetic():
+    entered_name = input("Enter your name: ").upper()
+    try:
+        nato_phonetic_list = [nato_alpha_dict[letter] for letter in entered_name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(nato_phonetic_list)
 
-entered_name = input("Enter your name: ").upper()
-nato_phonetic_list = [nato_alpha_dict[letter] for letter in entered_name]
-print(nato_phonetic_list)
+generate_phonetic()
