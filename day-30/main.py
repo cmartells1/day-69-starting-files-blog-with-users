feaa66948e640fa63d993a2ead262f4cc95ca63f
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-10 13:53:49 -0600

    raising your own exceptions

--- day-30/main.py
+++ day-30/main.py
@@ -1,23 +1,31 @@
 #FileNOtFound
 # with open("a_file.txt") as file:
 #     file.read()
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError: #You want to put the exception that you are trying to find just incase there is
-                         # more then one exception that could be thrown by the try block
-    file = open("a_file.txt", "w")
-    file.write("Print something in file")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist")
-else: #this code will excecute if there is no exceptions
-    content = file.read()
-    print(content)
-finally:# This code will run no matter what happens
-    file.close()
-    print("File was closed")
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError: #You want to put the exception that you are trying to find just incase there is
+#                          # more then one exception that could be thrown by the try block
+#     file = open("a_file.txt", "w")
+#     file.write("Print something in file")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else: #this code will excecute if there is no exceptions
+#     content = file.read()
+#     print(content)
+# finally:# This code will run no matter what happens
+#     raise KeyError("THis is an error i made up")
 
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3 :
+    raise ValueError("Human height should not be over 3 meters")
+
+bmi = weight / height ** 2
+
+print(bmi)
 
 #keyError
 # a_dictionary = {"key":"value"}
