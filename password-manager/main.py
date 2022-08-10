6902511957cdf4c82891e72bbab74b1098b6eb06
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-10 11:37:54 -0600

    password manager start - working with images and setting up the canvas

---
+++ password-manager/main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="logo.png")
+#x and y are position not size
+canvas.create_image(100,100,image=lock_img)
+canvas.grid()
+
+
+window.mainloop()
