2d9ca5255b90501d8616a0edbbe5ac33e73e5fb8
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-10 12:23:03 -0600

    creating the layout grid

--- password-manager/main.py
+++ password-manager/main.py
@@ -7,13 +7,35 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 lock_img = PhotoImage(file="logo.png")
 #x and y are position not size
 canvas.create_image(100,100,image=lock_img)
-canvas.grid()
+canvas.grid(column=1, row=0)
+
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_entry = Entry(width=35, justify="left")
+website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3, sticky="EW")
+
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3, sticky="EW")
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 
 window.mainloop()
