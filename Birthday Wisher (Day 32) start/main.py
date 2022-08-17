146b02c4ded29fa4cfc7a48c68386e214081d0bb
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-17 14:34:27 -0600

    Challenge - Send motivational quotes by email

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -12,11 +12,33 @@
 #                         )
 
 import datetime as dt
+import random
+import smtplib
+
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_week = now.weekday()
+# print(year)
+#
+# date_of_birth = dt.datetime(year=1985, month=10, day=3)
+
+MY_EMAIL = "martells.test@gmail.com"
+PASSWORD = "kbrjybvetpgzyhnh"
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-print(year)
+today = now.weekday()
+quote_list = []
 
-date_of_birth = dt.datetime(year=1985, month=10, day=3)
+if today == 2:
+    with open(file="quotes.txt", mode="r") as data:
+        quote_list = data.readlines()
+        quote = random.choice(quote_list)
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs="martells.test@yahoo.com",
+                            msg=f"Subject:Quote of the day\n\n{quote}"
+                            )
