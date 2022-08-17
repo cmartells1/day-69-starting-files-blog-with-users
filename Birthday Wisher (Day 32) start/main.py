61a9af1293326a29adde38095cdd302839151512
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-17 13:59:03 -0600

    SMTP start

---
+++ Birthday Wisher (Day 32) start/main.py
@@ -0,0 +1,15 @@
+# import smtplib
+#
+# my_email = "martells.test@gmail.com"
+# password = "kbrjybvetpgzyhnh"
+#
+# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="martells.test@yahoo.com",
+#                         msg="Subject:Hello\n\nThis is the body of my email"
+#                         )
+
+import datetime
+
