f5e9644e3765713a76d8ab0754e8a18ae9bcb9c2
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-22 16:23:42 -0600

    Class based Tkinter UI for quiz app

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -1,2 +1,32 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Quiz Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        self.score = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score.grid(column=1, row=0)
+
+        check_image = PhotoImage(file="images/true.png")
+        self.check_button = Button(image=check_image, highlightthickness=0)
+        self.check_button.grid(column=0, row=2)
+
+        x_image = PhotoImage(file="images/false.png")
+        self.x_button = Button(image= x_image, highlightthickness=0)
+        self.x_button.grid(column=1, row=2)
+
+        self.window.mainloop()
