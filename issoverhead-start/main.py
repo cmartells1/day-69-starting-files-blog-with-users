a9a1e08c390b920fa768e2bdf05577c0f988fb29
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-22 15:52:50 -0600

    Trivia questions api and escaping HTML  entities

--- issoverhead-start/main.py
+++ issoverhead-start/main.py
@@ -18,8 +18,6 @@ def is_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    print(iss_latitude)
-    print(iss_longitude)
     #Your position is within +5 or -5 degrees of the ISS position.
     if MY_LAT-5 <= iss_latitude <= MY_LAT + 5 and MY_LONG- 5 <= iss_longitude <= MY_LONG +5:
         return True
