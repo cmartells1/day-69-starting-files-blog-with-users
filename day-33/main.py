fff07d70ecf2d772976278127f7db9b32efccf10
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-22 14:46:45 -0600

    Understand API parameters

--- day-33/main.py
+++ day-33/main.py
@@ -1,13 +1,37 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 53.544388
+MY_LONG = -113.490929
+
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude,latitude)
+#
+# print(iss_position)
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+print(sunrise)
+print(sunset)
 
-iss_position = (longitude,latitude)
+time_now = datetime.now()
+print(time_now.hour)
 
-print(iss_position)
