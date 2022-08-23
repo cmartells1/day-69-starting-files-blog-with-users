bf647a35bd6d855d2cf3742658be86377cb61a32
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-23 14:19:46 -0600

    Python typing and showing the next question in the GUI

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -1,9 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    # Adding a datatype will help gain the methods from the class and gurantee that what is passed in is the
+    # correct data type
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -12,6 +16,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Quiz Text",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -29,4 +34,10 @@ class QuizInterface:
         self.x_button = Button(image= x_image, highlightthickness=0)
         self.x_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
