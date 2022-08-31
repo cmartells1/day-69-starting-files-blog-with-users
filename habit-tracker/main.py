0952c91ec409ff4e39b09797c39962f7853c5907
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-31 13:29:40 -0600

    Day 37 - Pixela start

---
+++ habit-tracker/main.py
@@ -0,0 +1,13 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": "ndfsipjfpwion23pn3ikpenr2pn3k",
+    "username": "cmartells",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
