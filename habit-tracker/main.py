5a8d191ef49ab72e578965d25451a1f11cc51b8d
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2022-08-31 13:44:13 -0600

    Day 37 step 2-3

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,5 +1,8 @@
 import requests
 
+USERNAME = "cmartells"
+TOKEN = "ndfsipjfpwion23pn3ikpenr2pn3k"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
     "token": "ndfsipjfpwion23pn3ikpenr2pn3k",
@@ -11,3 +14,18 @@ user_params = {
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_params = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+print(response.text)
