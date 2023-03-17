4aab8f6d8f04ffcf841b8583ab9d67d60a21c88c
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-03-17 14:14:16 -0600

    Update main.py

--- Web Development/Spotify_Playlist/main.py
+++ Web Development/Spotify_Playlist/main.py
@@ -6,15 +6,18 @@ from spotipy import SpotifyOAuth
 URL = "https://www.billboard.com/charts/hot-100/"
 CLIENT_ID = "29b9325024724a4d90e8a11691e568b4"
 CLIENT_SECRET = "0b687f9c80d642f9a904fc939b594f98"
-#
-# entered_date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-# response = requests.get(f"{URL}/{entered_date}")
-# billboard_web_page = response.text
-#
-# soup = BeautifulSoup(billboard_web_page, "html.parser")
-# song_name_h3 = soup.findAll(name='h3', class_='a-no-trucate')
-# song_list = [song.getText().strip() for song in song_name_h3]
-# print(song_list)
+
+
+entered_date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+year = entered_date.split('-')[0]
+print(year)
+response = requests.get(f"{URL}/{entered_date}")
+billboard_web_page = response.text
+
+soup = BeautifulSoup(billboard_web_page, "html.parser")
+song_name_h3 = soup.findAll(name='h3', class_='a-no-trucate')
+song_list = [song.getText().strip() for song in song_name_h3]
+print(song_list)
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth
