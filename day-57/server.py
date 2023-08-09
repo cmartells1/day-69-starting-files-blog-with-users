2566e46556d6292b28059f4e7da37903695fb3c0
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-08-09 13:42:17 -0600

    Jinga working with apis start

--- day-57/server.py
+++ day-57/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -10,6 +11,17 @@ def home():
     current_year = datetime.date.today().year
     return render_template('index.html', num=random_number, curr_year=current_year)
 
+@app.route('/guess/<name>')
+def age_gender(name):
+    age_response = requests.get(f"https://api.agify.io?name={name}")
+    age_data = age_response.json()
+    age = age_data['age']
+
+    gender_response = requests.get(f"https://api.genderize.io?name={name}")
+    gender_data = gender_response.json()
+    gender = gender_data['gender']
+
+    return render_template('age_gender.html', age=age, name=name, gender=gender)
 
 if __name__ == '__main__':
     app.run(debug=True)
