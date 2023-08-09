f46b3f48fbc7e38c71d0c11848a962ef9aa5f470
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-08-09 13:56:19 -0600

    multiline statments with jinja

--- day-57/server.py
+++ day-57/server.py
@@ -23,5 +23,12 @@ def age_gender(name):
 
     return render_template('age_gender.html', age=age, name=name, gender=gender)
 
+@app.route('/blog')
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
 if __name__ == '__main__':
     app.run(debug=True)
