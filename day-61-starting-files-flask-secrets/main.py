c4a82d0376b053f6d16e9b37eae9da46825bb871
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-08-15 00:17:47 -0600

    day 61 wtf form add validation

--- day-61-starting-files-flask-secrets/main.py
+++ day-61-starting-files-flask-secrets/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, length
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -14,8 +15,8 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 class MyForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[DataRequired(), Email(message="Invalid email address")])
+    password = PasswordField(label='Password', validators=[DataRequired(),length(min=8, message="Field must be at least 8 characters long")])
     submit = SubmitField(label='Log In')
 
 
@@ -26,9 +27,14 @@ app.secret_key = "wang"
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods= ["GET", "POST"])
 def login():
     form = MyForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=form)
 
 if __name__ == '__main__':
