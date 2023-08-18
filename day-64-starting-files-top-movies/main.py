934e3b896082829e7521173d27df7b46a887aed3
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-08-18 00:09:55 -0600

    Day 64 requirement 3

--- day-64-starting-files-top-movies/main.py
+++ day-64-starting-files-top-movies/main.py
@@ -65,6 +65,14 @@ def edit(movie_id):
         return redirect(url_for('home'))
     return render_template("edit.html", form=form, movie=movie)
 
+@app.route("/delete")
+def delete():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
