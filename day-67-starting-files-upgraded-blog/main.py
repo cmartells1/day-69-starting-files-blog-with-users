20a9b78f37ef9824c673ef2a4802bfbf5ef29662
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2023-08-28 01:12:52 -0600

    day 67 delete posts

--- day-67-starting-files-upgraded-blog/main.py
+++ day-67-starting-files-upgraded-blog/main.py
@@ -109,7 +109,12 @@ def edit_post(post_id):
         return redirect(url_for('show_post', post_id = post_to_edit.id))
     return render_template('make-post.html', post=post_to_edit, form=form)
 # TODO: delete_post() to remove a blog post from the database
-
+@app.route("/delete-post/<int:post_id>")
+def delete_post(post_id):
+    post = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
 def about():
